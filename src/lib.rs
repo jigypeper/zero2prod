e26fae4835d38925e4f2a4649841adbfe6783c8b
Ahmed Hagi <ahmedosman@mac.com>
2023-03-30 11:34:47 +0100

    added shell script for docker

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,25 @@ use std::net::TcpListener;
 
 use actix_web::{web, App, HttpServer, Responder, HttpResponse, dev::Server};
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    name: String,
+    email: String
+}
+
 async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
+    HttpResponse::Ok().finish()
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
     })
     .listen(listener)?
     .run();
